8d03c2e7d85cda314904d7b471a04c12d6d14ca1
bonheurisme <100709185+callmejippo@users.noreply.github.com>
2024-03-17 23:32:30 +0700

    feat(edit_activity): add DateTimePicker and define events(Edit image name, edit date time, save, cancel)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,43 +1,44 @@
-plugins {
-    alias(libs.plugins.androidApplication)
-}
-
-android {
-    namespace = "com.example.semsemgallery"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.semsemgallery"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    plugins {
+        alias(libs.plugins.androidApplication)
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    android {
+        namespace = "com.example.semsemgallery"
+        compileSdk = 34
+
+        defaultConfig {
+            applicationId = "com.example.semsemgallery"
+            minSdk = 28
+            targetSdk = 34
+            versionCode = 1
+            versionName = "1.0"
+
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-}
 
-dependencies {
-    implementation("com.squareup.picasso:picasso:2.5.2")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    implementation(libs.glide)
-}
+    dependencies {
+        implementation("com.squareup.picasso:picasso:2.5.2")
+        implementation("androidx.recyclerview:recyclerview:1.3.2")
+        implementation("de.hdodenhof:circleimageview:3.1.0")
+        implementation("com.github.Kunzisoft:Android-SwitchDateTimePicker:2.1")
+        implementation(libs.appcompat)
+        implementation(libs.material)
+        implementation(libs.activity)
+        implementation(libs.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.ext.junit)
+        androidTestImplementation(libs.espresso.core)
+        implementation(libs.glide)
+    }
