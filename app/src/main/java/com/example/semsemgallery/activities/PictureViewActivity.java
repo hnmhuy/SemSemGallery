f2bfcf9d4eedfdc4c7d77766037b706afad6c021
nkhy-dev-3112 <nkhy.dev@gmail.com>
2024-03-26 20:27:05 +0700

    feat: Handle onBackPressed in PictureViewActivity

--- app/src/main/java/com/example/semsemgallery/activities/PictureViewActivity.java
+++ app/src/main/java/com/example/semsemgallery/activities/PictureViewActivity.java
@@ -1,6 +1,8 @@
 package com.example.semsemgallery.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ import com.example.semsemgallery.models.Picture;
 import java.util.ArrayList;
 
 public class PictureViewActivity extends AppCompatActivity {
+    private com.google.android.material.appbar.MaterialToolbar topBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +33,20 @@ public class PictureViewActivity extends AppCompatActivity {
         ArrayList<Picture> pictureList = getIntent().getParcelableArrayListExtra("pictureList");
         int position = getIntent().getIntExtra("position", 0);
 
+
+        topBar = (com.google.android.material.appbar.MaterialToolbar) findViewById(R.id.activity_picture_view_topAppBar);
+
         ViewPager2 viewPager = findViewById(R.id.vp_image);
         PictureAdapter adapter = new PictureAdapter(this, pictureList, position);
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(position, false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // ====== Listener for the BackButton in the TopBar
+        topBar.setNavigationOnClickListener(v -> finish());
     }
 }
