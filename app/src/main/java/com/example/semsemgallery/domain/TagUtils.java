1c17b98612f296ae4ce4f4232e89cc6afc71b4d2
NhatPhi1804 <kenshiro.pn@gmail.com>
2024-04-10 15:03:43 +0700

    format: Remove redundant ag methods

--- app/src/main/java/com/example/semsemgallery/domain/TagUtils.java
+++ app/src/main/java/com/example/semsemgallery/domain/TagUtils.java
@@ -43,18 +43,6 @@ public class TagUtils extends SQLiteOpenHelper {
             + COLUMN_TAGID_PICTURETAG + " INTEGER,"
             + "PRIMARY KEY (" + COLUMN_PICTUREID + ", " + COLUMN_TAGID_PICTURETAG + ")"
             + ")";
-//    private static final String CREATE_TABLE_PICTURETAG = String.format(
-//            "CREATE TABLE %s(%s TEXT PRIMARY KEY,%s INTEGER PRIMARY)",
-//            TABLE_PICTURETAG,
-//            COLUMN_PICTUREID,
-//            COLUMN_TAGID_PICTURETAG);
-
-    // Script creates Table TAG
-//    private static final String CREATE_TABLE_TAG = String.format(
-//            "CREATE TABLE %s(%s INTEGER PRIMARY KEY AUTOINCREMENT,%s TEXT)",
-//            TABLE_TAG,
-//            COLUMN_TAGID,
-//            COLUMN_TAGNAME);
 
     // Constructor
     public TagUtils(Context context) {
@@ -88,7 +76,6 @@ public class TagUtils extends SQLiteOpenHelper {
 
     // Get Database
     public SQLiteDatabase myGetDatabase(Context context) {
-//        return this.getWritableDatabase();
         SQLiteDatabase db;
 
         File dbFile = context.getDatabasePath(DATABASE_NAME);
@@ -97,11 +84,8 @@ public class TagUtils extends SQLiteOpenHelper {
         } else {
             db = getWritableDatabase();
         }
-
         return db;
-
     }
-
 
     // Insert a new Tag
     public void insertTag(SQLiteDatabase db, String tagName) {
@@ -110,43 +94,7 @@ public class TagUtils extends SQLiteOpenHelper {
         db.insert(TABLE_TAG, null, values);
     }
 
-    public void insertPictureTag(SQLiteDatabase db, String pictureId, int tagId) {
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_PICTUREID, pictureId);
-        values.put(COLUMN_TAGID_PICTURETAG, tagId);
-        db.insert(TABLE_PICTURETAG, null, values);
-    }
-
-    public void deletePictureTag(SQLiteDatabase db, String pictureId) {
-        db.delete(TABLE_PICTURETAG, COLUMN_PICTUREID + " = ?", new String[]{pictureId});
-    }
-
-    public void deleteTag(SQLiteDatabase db, int tagId) {
-        db.delete(TABLE_TAG, COLUMN_TAGID + " = ?", new String[]{String.valueOf(tagId)});
-    }
-
-    public void updateTag(SQLiteDatabase db, int tagId, String tagName) {
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_TAGNAME, tagName);
-        db.update(TABLE_TAG, values, COLUMN_TAGID + " = ?", new String[]{String.valueOf(tagId)});
-    }
-
-    public void updatePictureTag(SQLiteDatabase db, String pictureId, int tagId) {
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_TAGID_PICTURETAG, tagId);
-        db.update(TABLE_PICTURETAG, values, COLUMN_PICTUREID + " = ?", new String[]{pictureId});
-    }
-
-    public Cursor getTag(SQLiteDatabase db, int tagId) {
-        return db.query(TABLE_TAG, null, COLUMN_TAGID + " = ?", new String[]{String.valueOf(tagId)}, null, null, null);
-    }
-
-    public Cursor getPictureTag(SQLiteDatabase db, String pictureId) {
-        return db.query(TABLE_PICTURETAG, null, COLUMN_PICTUREID + " = ?", new String[]{pictureId}, null, null, null);
-    }
-
     public ArrayList<Tag> getAllTags(SQLiteDatabase db) {
-//        return db.query(TABLE_TAG, null, null, null, null, null, null);
         ArrayList<Tag> tags = new ArrayList<>();
         String query = "SELECT * FROM " + TABLE_TAG;
         Cursor cursor = db.rawQuery(query, null);
@@ -164,51 +112,6 @@ public class TagUtils extends SQLiteOpenHelper {
         return tags;
     }
 
-    public ArrayList<Tag> getAllPictureTags(SQLiteDatabase db) {
-        ArrayList<Tag> tags = new ArrayList<>();
-        String query = "SELECT * FROM " + TABLE_TAG;
-        Cursor cursor = db.rawQuery(query, null);
-
-
-        if(cursor != null && cursor.moveToFirst()){
-            do{
-                @SuppressLint("Range") int id = cursor.getInt(cursor.getColumnIndex(COLUMN_TAGID));
-                @SuppressLint("Range") String tagName = cursor.getString(cursor.getColumnIndex(COLUMN_TAGNAME));
-                Tag tag = new Tag(id, tagName);
-                tags.add(tag);
-            }while (cursor.moveToNext());
-        }
-        cursor.close();
-        return tags;
-    }
-
-    public Cursor getTagByName(SQLiteDatabase db, String tagName) {
-        return db.query(TABLE_TAG, null, COLUMN_TAGNAME + " = ?", new String[]{tagName}, null, null, null);
-    }
-
-    public Cursor getPictureTagByTagId(SQLiteDatabase db, int tagId) {
-        return db.query(TABLE_PICTURETAG, null, COLUMN_TAGID_PICTURETAG + " = ?", new String[]{String.valueOf(tagId)}, null, null, null);
-    }
-
-    public Cursor getPictureTagByPictureId(SQLiteDatabase db, String pictureId) {
-        return db.query(TABLE_PICTURETAG, null, COLUMN_PICTUREID + " = ?", new String[]{pictureId}, null, null, null);
-    }
-
-    public Cursor getTagIdByTagName(SQLiteDatabase db, String tagName) {
-        return db.query(TABLE_TAG, new String[]{COLUMN_TAGID}, COLUMN_TAGNAME + " = ?", new String[]{tagName}, null, null, null);
-    }
-
-    public Cursor getTagNameByTagId(SQLiteDatabase db, int tagId) {
-        return db.query(TABLE_TAG, new String[]{COLUMN_TAGNAME}, COLUMN_TAGID + " = ?", new String[]{String.valueOf(tagId)}, null, null, null);
-    }
-
-    public Cursor getPictureIdByTagId(SQLiteDatabase db, int tagId) {
-        return db.query(TABLE_PICTURETAG, new String[]{COLUMN_PICTUREID}, COLUMN_TAGID_PICTURETAG + " = ?", new String[]{String.valueOf(tagId)}, null, null, null);
-    }
-
-    public Cursor getTagIdByPictureId(SQLiteDatabase db, String pictureId) {
-        return db.query(TABLE_PICTURETAG, new String[]{COLUMN_TAGID_PICTURETAG}, COLUMN_PICTUREID + " = ?", new String[]{pictureId}, null, null, null);
-    }
     public Tag searchTag(SQLiteDatabase db, String keyword){
         String selectionArgs = "%" + keyword + "%";
 
