4c393b53405040d063f8fa95ebebe2ee5890d5b2
bonheurisme <100709185+callmejippo@users.noreply.github.com>
2024-04-19 12:24:21 +0700

    fix: delete datetime picker library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,6 @@ IMGLY.configure() {
         implementation("com.google.android.material:material:1.3.0-alpha02")
         implementation("androidx.recyclerview:recyclerview:1.3.2")
         implementation("de.hdodenhof:circleimageview:3.1.0")
-        implementation("com.github.Kunzisoft:Android-SwitchDateTimePicker:2.1")
         implementation("com.google.mlkit:text-recognition:16.0.0");         // To recognize Latin script
         implementation("com.google.mlkit:language-id:17.0.5");
         implementation("com.google.mlkit:image-labeling:17.0.8");
