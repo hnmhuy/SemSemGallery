5c2b13f9a28cadb4405d69ca216f5530db07c4e0
Thế Nghĩa <nghiask2018@gmail.com>
2024-04-21 21:50:12 +0700

    feat: create exception dialog

--- app/src/main/java/com/example/semsemgallery/activities/main2/fragment/AlbumsFragment.java
+++ app/src/main/java/com/example/semsemgallery/activities/main2/fragment/AlbumsFragment.java
@@ -82,7 +82,29 @@ public class AlbumsFragment extends Fragment implements GridModeListener {
 
     // ====== Activity Result Launcher for Photo Picker
     private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == AppCompatActivity.RESULT_OK && result.getData() != null) {
+                        selectedImages.clear();
 
+                        int count = result.getData().getClipData().getItemCount();
+                        for (int i = 0; i < count; i++) {
+                            Uri imageUri = result.getData().getClipData().getItemAt(i).getUri();
+                            selectedImages.add(imageUri);
+                        }
+
+                        // ====== Open AlbumHandler Dialog after picking images
+                        if (!selectedImages.isEmpty()) {
+                            showAlbumHandlerDialog();
+                        }
+                        else {
+                            Toast.makeText(context, "No images selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
     );
 
     @Override
@@ -96,18 +118,24 @@ public class AlbumsFragment extends Fragment implements GridModeListener {
         loader = new AlbumLoader(context) {
             @Override
             public void onProcessUpdate(Album... albums) {
-
+                albumArrayList.add(albums[0]);
+                observedObj.addData(albums[0]);
+                Log.d("AlbumLoader", albums[0].getAlbumId() + " - " + albums[0].getName() + " - " + albums[0].getWallPath());
             }
 
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void postExecute(Void res) {
-
+                super.postExecute(res);
+                adapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             public void preExecute(String... strings) {
-
+                super.preExecute(strings);
+                observedObj.getObservedObjects().clear();
+                albumArrayList.clear();
             }
         };
     }
@@ -140,6 +168,20 @@ public class AlbumsFragment extends Fragment implements GridModeListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.i("AlbumFragment", "On create view");
 
+        // ====== Render View
+        View view = inflater.inflate(R.layout.fragment_albums, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.album_recycler);
+        GridLayoutManager manager = new GridLayoutManager(getActivity(), 3);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        progressBar = view.findViewById(R.id.progressBar);
+        // ====== Get TopBar
+        topBar = view.findViewById(R.id.fragment_albums_topAppBar);
+        selectingTopBar = view.findViewById(R.id.fragment_albums_topAppBarSelecting);
+        bottomAction = view.findViewById(R.id.bottomActions);
+        SetActionBottomFunctions();
+        SetActionTopFunctions();
+        return view;
     }
 
     // ====== Set Listener for Icon in Top Bar right here
@@ -153,5 +195,277 @@ public class AlbumsFragment extends Fragment implements GridModeListener {
         loader.execute();
     }
 
+
+    // ====== Show Input Dialog
+    private void showInputDialog() {
+        View dialogView = getLayoutInflater().inflate(R.layout.component_input_dialog, null);
+
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(requireContext())
+                .setView(dialogView);
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
+        TextView cancelBtn = dialogView.findViewById(R.id.component_input_dialog_cancel);
+        TextView createBtn = dialogView.findViewById(R.id.component_input_dialog_create);
+        EditText inputName = dialogView.findViewById(R.id.component_input_dialog_name);
+
+        // ====== Listener for CancelButton in InputDialog clicked
+        cancelBtn.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        // ====== Listener for CreateButton in InputDialog clicked
+        createBtn.setOnClickListener(v -> {
+            newAlbumName = inputName.getText().toString();
+            dialog.dismiss();
+
+            // If album does not exist -> Open Photo Picker
+            if (!AlbumHandler.checkAlbumExists(requireContext(), newAlbumName)) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                photoPickerIntent.setType("image/*");
+                photoPickerIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                activityResultLauncher.launch(photoPickerIntent);
+            }
+        });
+    }
+
+    // ====== Show Album Handler Dialog
+    private void showAlbumHandlerDialog() {
+        View dialogView = getLayoutInflater().inflate(R.layout.component_album_handler_dialog, null);
+
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(context).setView(dialogView);
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
+        TextView cancelBtn = dialogView.findViewById(R.id.component_album_handler_dialog_cancel);
+        TextView copyBtn = dialogView.findViewById(R.id.component_album_handler_dialog_copy);
+        TextView moveBtn = dialogView.findViewById(R.id.component_album_handler_dialog_move);
+
+        // ====== Listener for CancelButton in AlbumHandlerDialog clicked
+        cancelBtn.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        // ====== Listener for CopyButton in AlbumHandlerDialog clicked
+        copyBtn.setOnClickListener(v -> {
+            dialog.dismiss();
+
+            AlertDialog loadingDialog = myLoadingDialog();
+            loadingDialog.show(); // Show Loading Dialog
+            TextView dialogTitle = loadingDialog.findViewById(R.id.component_loading_dialog_title);
+            dialogTitle.setText("Copying to " + newAlbumName);
+
+            // Create a listener for completion & set progress
+            AlbumHandler.OnLoadingListener loadingListener = new AlbumHandler.OnLoadingListener() {
+                @Override
+                public void onLoadingComplete() {
+                    loadingDialog.dismiss();
+                    loader.execute();
+                }
+
+                @Override
+                public void onLoadingProgressUpdate(int progress) {
+                    ProgressBar progressBar = loadingDialog.findViewById(R.id.component_loading_dialog_progressBar);
+                    mHandler.post(() -> {
+                        progressBar.setProgress(progress);
+                    });
+                }
+            };
+
+            AlbumHandler.copyImagesToAlbumHandler(context, selectedImages, newAlbumName, loadingListener);
+        });
+
+
+        // ====== Listener for MoveButton in AlbumHandlerDialog clicked
+        moveBtn.setOnClickListener(v -> {
+            dialog.dismiss();
+
+            AlertDialog loadingDialog = myLoadingDialog();
+            loadingDialog.show(); // Show Loading Dialog
+            TextView dialogTitle = loadingDialog.findViewById(R.id.component_loading_dialog_title);
+            dialogTitle.setText("Moving to " + newAlbumName);
+
+            // Create a listener for completion & set progress
+            AlbumHandler.OnLoadingListener loadingListener = new AlbumHandler.OnLoadingListener() {
+                @Override
+                public void onLoadingComplete() {
+                    loadingDialog.dismiss();
+                    loader.execute();
+                }
+
+                @Override
+                public void onLoadingProgressUpdate(int progress) {
+                    ProgressBar progressBar = loadingDialog.findViewById(R.id.component_loading_dialog_progressBar);
+                    mHandler.post(() -> {
+                        progressBar.setProgress(progress);
+                    });
+                }
+            };
+
+            AlbumHandler.moveImagesToAlbumHandler(context, selectedImages, newAlbumName, loadingListener);
+        });
+    }
+
+    // ==== Just only able to init Dialog in Fragment/Activity
+    private AlertDialog myLoadingDialog() {
+        View dialogView = getLayoutInflater().inflate(R.layout.component_loading_dialog, null);
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(context).setView(dialogView);
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.setCanceledOnTouchOutside(false);
+
+        Button cancelButton = dialogView.findViewById(R.id.component_loading_dialog_cancelButton);
+        cancelButton.setOnClickListener(v -> {
+            AlbumHandler.stopHandling();
+        });
+
+        return dialog;
+    }
+
+    @Override
+    public void onModeChange(GridModeEvent event) {
+        //== Send the signal to main activity
+        mainActivity.sendMsgToMain("AlbumsFragment", event.getGridMode().toString());
+        topBar.setVisibility(GridMode.SELECTING == event.getGridMode() ? View.INVISIBLE : View.VISIBLE);
+        selectingTopBar.setVisibility(GridMode.SELECTING == event.getGridMode() ? View.VISIBLE : View.INVISIBLE);
+        bottomAction.setVisibility(GridMode.SELECTING == event.getGridMode() ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void onSelectionChange(GridModeEvent event) {
+        long value = observedObj.getNumberOfSelected();
+        selectingTopBar.setTitle(value == 0 ? getResources().getString(R.string.select_items) : String.valueOf(value) + " selected");
+    }
+
+    @Override
+    public void onSelectingAll(GridModeEvent event) {
+        selectingTopBar.setTitle(event.getNewSelectionForAll() ? String.valueOf(observedObj.getDataSize()) + " selected" : getString(R.string.select_items));
+    }
+
+    private void SetActionTopFunctions() {
+        // ====== Listener for AddIcon in TopBar clicked
+        topBar.setOnMenuItemClickListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.add) {
+                showInputDialog();
+                return true;
+            } else if (id == R.id.search) {
+                startActivity(new Intent(context.getApplicationContext(), SearchViewActivity.class));
+                return true;
+            } else if (id == R.id.edit) {
+                observedObj.setGridMode(GridMode.SELECTING);
+                return true;
+            } else if (id == R.id.select_all) {
+                observedObj.setGridMode(GridMode.SELECTING);
+                observedObj.fireSelectionChangeForAll(true);
+                isSelectingAll = true;
+                return true;
+            } else return false;
+        });
+    }
+
+    private void SetActionBottomFunctions() {
+        Button btnDelete = bottomAction.findViewById(R.id.btnDelete);
+        Button btnAll = bottomAction.findViewById(R.id.btnAll);
+
+        btnAll.setOnClickListener((v) -> {
+            observedObj.fireSelectionChangeForAll(!isSelectingAll);
+            isSelectingAll = !isSelectingAll;
+            btnAll.setText(isSelectingAll ? "Unselect All" : "Select All");
+        });
+
+        btnDelete.setOnClickListener((v) -> {
+            if (observedObj.getNumberOfSelected() < 1) {
+                Toast.makeText(context, "No albums selected", Toast.LENGTH_SHORT).show();
+            } else {
+                showDeleteConfirmDialog();
+            }
+        });
+    }
+
+    // ====== Show Delete Confirm Dialog
+    private void showDeleteConfirmDialog() {
+        View dialogView = getLayoutInflater().inflate(R.layout.component_confirm_dialog, null);
+        MaterialAlertDialogBuilder dialogBuilder = new MaterialAlertDialogBuilder(context).setView(dialogView);
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
+        TextView cancelBtn = dialogView.findViewById(R.id.component_confirm_dialog_cancel);
+        TextView deleteBtn = dialogView.findViewById(R.id.component_confirm_dialog_confirm);
+        TextView title = dialogView.findViewById(R.id.component_confirm_dialog_title);
+        TextView description = dialogView.findViewById(R.id.component_confirm_dialog_description);
+        long numberOfSelectedAlbums = observedObj.getNumberOfSelected();
+        if (numberOfSelectedAlbums > 1) {
+            title.setText("Move " + numberOfSelectedAlbums + " albums to Trash");
+            description.setText("Confirm move selected albums to the Trash?");
+        } else if (numberOfSelectedAlbums == 1) {
+            title.setText("Move " + numberOfSelectedAlbums + " album to Trash");
+            description.setText("Confirm move selected album to the Trash?");
+        }
+
+        // ====== Listener for CancelButton in Delete Confirm Dialog clicked
+        cancelBtn.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        // ====== Listener for DeleteButton in Delete Confirm Dialog clicked
+        deleteBtn.setOnClickListener(v -> {
+            dialog.dismiss();
+
+            // ======== Show Loading Dialog
+            AlertDialog loadingDialog = myLoadingDialog();
+            loadingDialog.show(); // Show Loading Dialog
+            TextView dialogTitle = loadingDialog.findViewById(R.id.component_loading_dialog_title);
+            dialogTitle.setText("Retrieving data");
+            final ProgressBar progressBar = loadingDialog.findViewById(R.id.component_loading_dialog_progressBar);
+            final ContentResolver resolver = context.getContentResolver();
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.IS_TRASHED, 1);
+            List<Picture> deleteData = new ArrayList<>();
+            List<Album> albumList = observedObj.getSelectedItems();
+            PictureLoader pictureLoader = new PictureLoader(context) {
+                @Override
+                public void onProcessUpdate(Picture... pictures) {
+                    deleteData.add(pictures[0]);
+                }
+
+                @Override
+                public void postExecute(Boolean res) {
+                    int current = progressBar.getProgress();
+                    current += (int) (100 / albumList.size());
+                    progressBar.setProgress(current);
+                    if (current >= 100) {
+                        // Loading complete
+                        dialogTitle.setText("Trashing pictures....");
+                        progressBar.setProgress(0);
+                        new Thread(() -> {
+                            for (Picture p : deleteData) {
+                                Uri uri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, p.getPictureId());
+                                if (GarbagePictureCollector.trashPicture(resolver, uri, values)) {
+                                    mHandler.post(() -> {
+                                        progressBar.setProgress(progressBar.getProgress() + (100 / deleteData.size()));
+                                    });
+                                }
+                            }
+
+                            mHandler.post(() -> {
+                                loadingDialog.dismiss();
+                                observedObj.setGridMode(GridMode.NORMAL);
+                                loader.execute();
+                            });
+
+                        }).start();
+                    }
+                }
+            };
+
+            for (Album album : albumList) {
+                pictureLoader.execute(PictureLoadMode.BY_ALBUM.toString(), album.getAlbumId());
+            }
+
+        });
+    }
 
 }
